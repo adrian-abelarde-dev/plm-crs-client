962094d43d857dcf96db892b334183852b75ec6d
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-07 12:57:59 +0800

    [Fix][Adrian] Add `next-branch-env`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+const withBranchEnv = require('next-branch-env')({ expose: 'BRANCH' })
+process.env.NEXTAUTH_URL = `https://${process.env.BRANCH}.${process.env.ROOT_HOST}`
+
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withBranchEnv(nextConfig)
