9c6ab60e284f9329354ccd4e512308bf9d79abd0
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-07 13:23:26 +0800

    [Fix][Adrian] Revert

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-/** @type {import('next').NextConfig} */
-const withBranchEnv = require('next-branch-env')({ expose: 'BRANCH' })
-process.env.NEXTAUTH_URL = `https://${process.env.BRANCH}.${process.env.ROOT_HOST}`
-
 const nextConfig = {}
 
-module.exports = withBranchEnv(nextConfig)
+module.exports = nextConfig
