de0580d48acea31c82a549f1bd2527eecd6cff19
urquico <kurtjacob.urquico@gmail.com>
2023-11-07 22:36:42 +0800

    [Refactor][Kurt] - Removed head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
-            <head></head>
             <body className={inter.className}>
                 <Providers>{children}</Providers>
             </body>
