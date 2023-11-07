aa28360c9ae982d88b99cb65ae27fc07da648aee
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-07 12:17:53 +0800

    [Fix][Adrian] Fix redirect uri prod

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,6 +5,7 @@ const handler = NextAuth({
     // Configure one or more authentication providers
     providers: [
         AzureADProvider({
+            id: 'azure-ad',
             clientId: process.env.AZURE_AD_CLIENT_ID,
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
             tenantId: process.env.AZURE_AD_TENANT_ID,
