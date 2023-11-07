14a4dab7cefae6b3cbe3975dcb89d9afaecdc522
urquico <kurtjacob.urquico@gmail.com>
2023-11-07 16:42:20 +0800

    [Fix][Kurt] - Removed mantine css

--- src/components/component/providers.jsx
+++ src/components/component/providers.jsx
@@ -1,15 +1,10 @@
 'use client';
 
-import { MantineProvider } from '@mantine/core';
 import { SessionProvider } from 'next-auth/react';
 import React from 'react';
 
 const Providers = ({ children }) => {
-    return (
-        <SessionProvider>
-            <MantineProvider>{children}</MantineProvider>
-        </SessionProvider>
-    );
+    return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default Providers;
