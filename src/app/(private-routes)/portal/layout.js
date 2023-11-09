ce2316a8d2fcbe65a16c9ef3f778026c8a745e24
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-10 07:57:23 +0800

    [Fix][Adrian] Fix portal slow loading and not rendering in layout.js portal

--- src/app/(private-routes)/portal/layout.js
+++ src/app/(private-routes)/portal/layout.js
@@ -1,6 +1,9 @@
 import AuthProvider from '@/components/component/auth';
 import Providers from '@/components/component/providers';
+import { getServerSession } from 'next-auth/next';
 import { Inter } from 'next/font/google';
+
+import { authOptions } from './api/auth/[...nextauth]/route';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,13 +13,14 @@ export const metadata = {
     'PLM - CRS is a computerized registration system for the Pamantasan ng Lungsod ng Maynila. Developed by batch 2023 - 2024 BSCS 4 - 1',
 };
 
-export default function PrivateRouteRootLayoutPortal({ children }) {
+export default async function PrivateRouteRootLayoutPortal({ children }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang='en'>
       <body className={inter.className}>
         {/* AuthProvider added for private routes */}
         <AuthProvider accessLevel='private'>
-          <Providers>{children}</Providers>
+          <Providers session={session}>{children}</Providers>
         </AuthProvider>
       </body>
     </html>
