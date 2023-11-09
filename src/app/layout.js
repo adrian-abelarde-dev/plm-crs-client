277349b31f20640090f631533cfb9dfe15835332
adrian-abelarde-dev <150082811+adrian-abelarde-dev@users.noreply.github.com>
2023-11-10 07:54:13 +0800

    [Fix][Adrian] Fix portal slow loading and not rendering (#35)
    Co-authored-by: drianlarde <aadabelarde2020@plm.edu.ph>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import Providers from '@/components/component/providers';
+import { getServerSession } from 'next-auth/next';
 import { Inter } from 'next/font/google';
 
+import { authOptions } from './api/auth/[...nextauth]/route';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,11 +13,12 @@ export const metadata = {
     'PLM - CRS is a computerized registration system for the Pamantasan ng Lungsod ng Maynila. Developed by batch 2023 - 2024 BSCS 4 - 1',
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers session={session}>{children}</Providers>
       </body>
     </html>
   );
