85ec04e4bffed551d971a19b75bccd001e290742
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-10 07:52:09 +0800

    [Fix][Adrian] Fix portal slow loading and not rendering

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import AzureADProvider from 'next-auth/providers/azure-ad';
 
-const handler = NextAuth({
+export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     AzureADProvider({
@@ -40,6 +40,8 @@ const handler = NextAuth({
       return session;
     },
   },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
