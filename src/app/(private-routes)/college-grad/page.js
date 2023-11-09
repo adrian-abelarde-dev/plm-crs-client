5866745fdb7741d5a999ff00033ae51815667691
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-10 06:22:38 +0800

    [Misc][Adrian] Add empty pages for missing pages in portal

---
+++ src/app/(private-routes)/college-grad/page.js
@@ -0,0 +1,5 @@
+const CollegeGradPage = () => {
+  return <></>;
+};
+
+export default CollegeGradPage;
---
+++ src/app/(private-routes)/faculty/page.js
@@ -0,0 +1,5 @@
+const FacultyPage = () => {
+  return <></>;
+};
+
+export default FacultyPage;
