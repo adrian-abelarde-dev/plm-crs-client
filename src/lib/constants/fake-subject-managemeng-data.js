90500f3d39bdfc127c39750d2bd07617e9167fdb
urquico <kurtjacob.urquico@gmail.com>
2023-11-11 23:45:04 +0800

    [Feature][Kurt] - Addition of fake data subject

---
+++ src/lib/constants/fake-subject-managemeng-data.js
@@ -0,0 +1,103 @@
+export const collegeGradSubjectManagementTemplate = [
+  {
+    accessorKey: 'subjectCode',
+    id: 'subjectCode',
+    header: 'Subject Code',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'subjectName',
+    id: 'subjectName',
+    header: 'Subject Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'units',
+    id: 'units',
+    header: 'Units',
+    filterVariant: 'fuzzy',
+  },
+];
+
+export const collegeGradSubjectManagementData = [
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-456',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-789',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-456',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-789',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-456',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-789',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-456',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-789',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-456',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-789',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+  {
+    subjectCode: 'GYM-123',
+    subjectName: 'Juan Dela Cruz',
+    units: '3.0',
+  },
+];
