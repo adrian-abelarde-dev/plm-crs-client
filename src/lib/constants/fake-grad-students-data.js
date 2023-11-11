f5684c37502287ae6ca970b1ff285cb14e579e93
urquico <kurtjacob.urquico@gmail.com>
2023-11-11 22:01:45 +0800

    [Feature][Kurt] - Addition of fake data

---
+++ src/lib/constants/fake-grad-students-data.js
@@ -0,0 +1,106 @@
+export const collegeGradStudentsTemplate = [
+  {
+    accessorKey: 'studentNumber',
+    id: 'studentNumber',
+    header: 'Student No.',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'firstName',
+    id: 'firstName',
+    header: 'First Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'middleName',
+    id: 'middleName',
+    header: 'Middle Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'lastName',
+    id: 'lastName',
+    header: 'Last Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'registrationCode',
+    id: 'registrationCode',
+    header: 'Registration Code',
+    filterVariant: 'fuzzy',
+  },
+];
+
+// data
+export const collegeGradStudentsData = [
+  {
+    studentNumber: '2018-00001',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'Regular',
+  },
+  {
+    studentNumber: '2018-00002',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'LOA',
+  },
+  {
+    studentNumber: '2018-00003',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00004',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00005',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00006',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00007',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00008',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00009',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+  {
+    studentNumber: '2018-00010',
+    firstName: 'Juan',
+    middleName: 'Dela',
+    lastName: 'Cruz',
+    registrationCode: 'HD',
+  },
+];
