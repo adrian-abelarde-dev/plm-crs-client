956eeae8ff913f242724adab3fa0141de68bcf2b
urquico <kurtjacob.urquico@gmail.com>
2023-11-11 18:35:39 +0800

    [Feature][Kurt] - Added Activity page on college grad

---
+++ src/lib/constants/fake-college-activities.js
@@ -0,0 +1,67 @@
+export const fakeActivitiesTemplate = [
+  {
+    accessorKey: 'activities',
+    id: 'activities',
+    header: 'Activity Title',
+    filterVariant: 'fuzzy',
+    size: 320,
+  },
+
+  {
+    accessorKey: 'dateStart',
+    id: 'dateStart',
+    header: 'Date Start',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'dateEnd',
+    id: 'dateEnd',
+    header: 'Date End',
+    filterVariant: 'fuzzy',
+  },
+];
+
+export const fakeCollegeActivities = [
+  {
+    id: '1',
+    activities: 'Encoding of Grades',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '2',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '3',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '4',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '5',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '6',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+  {
+    id: '7',
+    activities: 'Enrollment Period',
+    dateStart: 'September 1, 2023',
+    dateEnd: 'September 23, 2023',
+  },
+];
