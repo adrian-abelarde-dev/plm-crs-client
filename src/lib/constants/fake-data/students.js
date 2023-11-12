3fd73046f123e4359e8c161451c74c31fcee347b
drianlarde <aadabelarde2020@plm.edu.ph>
2023-11-12 18:57:59 +0800

    [Feature][Adrian] Add admin students page

---
+++ src/lib/constants/fake-data/students.js
@@ -0,0 +1,42 @@
+export const fakeStudents = [
+  {
+    userid: '2018-00001',
+    fullname: 'John Doe',
+  },
+  {
+    userid: '2018-00002',
+    fullname: 'Jane Doe',
+  },
+  {
+    userid: '2018-00003',
+    fullname: 'Alice Johnson',
+  },
+  {
+    userid: '2018-00004',
+    fullname: 'Bob Smith',
+  },
+  {
+    userid: '2018-00005',
+    fullname: 'Eva Martinez',
+  },
+  {
+    userid: '2018-00006',
+    fullname: 'Michael Brown',
+  },
+  {
+    userid: '2018-00007',
+    fullname: 'Sophia Davis',
+  },
+  {
+    userid: '2018-00008',
+    fullname: 'Daniel Wilson',
+  },
+  {
+    userid: '2018-00009',
+    fullname: 'Olivia Lee',
+  },
+  {
+    userid: '2018-00010',
+    fullname: 'Matthew Taylor',
+  },
+];
