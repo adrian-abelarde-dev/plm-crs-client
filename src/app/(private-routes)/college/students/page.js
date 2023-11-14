f0cb805c5df950bd94df09c5f9818728c5cf9f86
urquico <kurtjacob.urquico@gmail.com>
2023-11-14 21:39:13 +0800

    [Fix][Kurt] - Resolved build error

--- src/app/(private-routes)/college/students/page.js
+++ src/app/(private-routes)/college/students/page.js
@@ -1,9 +1,7 @@
-function CollegeStudents() {
-  return <></>;
-import StudentsCollegeTable from "@/components/component/admin/students-college-table";
+import StudentsCollegeTable from '@/components/component/admin/students-college-table';
 
 function StudentsPage() {
-  return(
+  return (
     <main className='w-full p-6'>
       <StudentsCollegeTable />
     </main>
