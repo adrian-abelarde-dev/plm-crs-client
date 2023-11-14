4aa59c645ec40d75f59063df182bb21722b7655f
AC Salazar <accsalazar2020@plm.edu.ph>
2023-11-14 13:53:33 +0800

    [Feature][Althea] Edited sidebar

--- src/components/component/college/college-sections.jsx
+++ src/components/component/college/college-sections.jsx
@@ -121,223 +121,222 @@ export const collegeSection = [
   { label: '5', value: '5' },
 ];
 
-function ArchiveSectionUndergrad({ disabled }) {
-  return (
-    <Button
-      disabled={disabled}
-      className='text-zinc-900 justify-between hover:bg-zinc-100'
-      variant='outline'
-    >
-      <ArchiveIcon className='inline-block w-4 h-4 mr-2' />
-      Archive
-    </Button>
-  );
-}
-
-function EditSectionUndergrad({ disabled }) {
-  const editSectionForm = useForm({
-    resolver: zodResolver(UserSchema),
-    defaultValues: {
-      userSchemaDefaultValues,
-    },
-  });
-
-  function onSubmit(values) {
-    console.log(values);
-  }
-
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button
-          disabled={disabled}
-          className='text-zinc-900 justify-between hover:bg-zinc-100'
-          variant='outline'
-        >
-          <Edit className='w-4 h-4 mr-2' />
-          Edit
-        </Button>
-      </DialogTrigger>
-      <DialogContent className='sm:max-w-[500px]'>
-        <DialogHeader>
-          <DialogTitle>Edit Individual Section</DialogTitle>
-        </DialogHeader>
-
-        <Form {...editSectionForm}>
-          <form onSubmit={editSectionForm.handleSubmit(onSubmit)}>
-            {/* Content */}
-            <div className='flex flex-col gap-2'>
-              {/* Section ID */}
-              <InputFormField
-                disabled={true}
-                form={editSectionForm}
-                title='Section ID'
-                placeholder='CETBSCS0401'
-                fieldName='sectionId'
-                badge={<Badge variant='outline'>Auto-generated</Badge>}
-              />
-
-              {/* Department */}
-              <SelectFormField
-                form={editSectionForm}
-                content={collegeDepartments}
-                title='Department'
-                placeholder='Select department...'
-                fieldName='collegeDepartment'
-              />
-
-              {/* Programs */}
-              <SelectFormField
-                form={editSectionForm}
-                content={collegePrograms}
-                title='Program'
-                placeholder='Select program...'
-                fieldName='collegeProgram'
-              />
-
-              {/* Year Level and Section */}
-              <section className='w-full flex gap-2 justify-items-start'>
-                {/* Year Level */}
-                <SelectFormField
-                  form={editSectionForm}
-                  content={collegeYear}
-                  title='Year Level'
-                  placeholder='Select year level...'
-                  fieldName='collegeYear'
-                />
-                <Label className='flex items-center'>-</Label>
-                {/* Section */}
-                <SelectFormField
-                  form={editSectionForm}
-                  content={collegeSection}
-                  title='Section'
-                  placeholder='Select section...'
-                  fieldName='collegeSection'
-                />
-              </section>
-
-              {/* Checkbox for Confirmation */}
-              <div className='items-top flex space-x-2 pt-2'>
-                <Checkbox id='confirm' />
-                <div className='grid gap-1.5 leading-none'>
-                  <label
-                    htmlFor='confirm'
-                    className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                  >
-                    Are you sure?
-                  </label>
-                  <p className='text-sm text-muted-foreground'>
-                    Double check if all inputs are correct to make sure there
-                    are no input errors.
-                  </p>
-                </div>
-              </div>
-
-              <DialogFooter className='w-full flex justify-end mt-4'>
-                <DialogClose asChild>
-                  <Button variant='outline'>Cancel</Button>
-                </DialogClose>
-                <Button type='submit'>Save Section</Button>
-              </DialogFooter>
-            </div>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
-  );
-}
-
-function AddSectionUndergrad() {
-  const addSectionForm = useForm({
-    resolver: zodResolver(UserSchema),
-    defaultValues: {
-      userSchemaDefaultValues,
-    },
-  });
-
-  function onSubmit(values) {
-    console.log(values);
-  }
-
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button>Add Section</Button>
-      </DialogTrigger>
-      <DialogContent className='sm:max-w-[500px]'>
-        <DialogHeader>
-          <DialogTitle>Add Section</DialogTitle>
-        </DialogHeader>
-
-        <Form {...addSectionForm}>
-          <form onSubmit={addSectionForm.handleSubmit(onSubmit)}>
-            {/* Content */}
-            <div className='flex flex-col gap-2'>
-              {/* Section ID */}
-              <InputFormField
-                disabled={true}
-                form={addSectionForm}
-                title='Section ID'
-                placeholder='CETBSCS0401'
-                fieldName='sectionId'
-                badge={<Badge variant='outline'>Auto-generated</Badge>}
-              />
-
-              {/* Year Level and Section */}
-              <section className='w-full flex gap-2 justify-items-start'>
-                {/* Year Level */}
-                <SelectFormField
-                  form={addSectionForm}
-                  content={collegeYear}
-                  title='Year Level'
-                  placeholder='Select year level...'
-                  fieldName='collegeYear'
-                />
-                <Label className='flex items-center'>-</Label>
-                {/* Section */}
-                <SelectFormField
-                  form={addSectionForm}
-                  content={collegeSection}
-                  title='Section'
-                  placeholder='Select section...'
-                  fieldName='collegeSection'
-                />
-              </section>
-
-              {/* Checkbox for Confirmation */}
-              <div className='items-top flex space-x-2 pt-2'>
-                <Checkbox id='confirm' />
-                <div className='grid gap-1.5 leading-none'>
-                  <label
-                    htmlFor='confirm'
-                    className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                  >
-                    Are you sure?
-                  </label>
-                  <p className='text-sm text-muted-foreground'>
-                    Double check if all inputs are correct to make sure there
-                    are no input errors.
-                  </p>
-                </div>
-              </div>
-
-              <DialogFooter className='w-full flex justify-end mt-4'>
-                <Button type='submit'>Save Section</Button>
-              </DialogFooter>
-            </div>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
-  );
-}
-
 function CollegeSectionsPage() {
   const [selectedSection, setSelectedSection] = useState({});
 
   useEffect(() => {
     console.log({ selectedSection });
   }, [selectedSection]);
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  function ArchiveSectionUndergrad({ disabled }) {
+    return (
+      <Button
+        disabled={disabled}
+        className='text-zinc-900 justify-between hover:bg-zinc-100'
+        variant='outline'
+      >
+        <ArchiveIcon className='inline-block w-4 h-4 mr-2' />
+        Archive
+      </Button>
+    );
+  }
+
+  function EditSectionUndergrad({ disabled }) {
+    const editSectionForm = useForm({
+      resolver: zodResolver(UserSchema),
+      defaultValues: {
+        userSchemaDefaultValues,
+      },
+    });
+
+    function onSubmit(values) {
+      console.log(values);
+    }
+
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            disabled={disabled}
+            className='text-zinc-900 justify-between hover:bg-zinc-100'
+            variant='outline'
+          >
+            <Edit className='w-4 h-4 mr-2' />
+            Edit
+          </Button>
+        </DialogTrigger>
+        <DialogContent className='sm:max-w-[500px]'>
+          <DialogHeader>
+            <DialogTitle>Edit Individual Section</DialogTitle>
+          </DialogHeader>
+          <Form {...editSectionForm}>
+            <form onSubmit={editSectionForm.handleSubmit(onSubmit)}>
+              {/* Content */}
+              <div className='flex flex-col gap-2'>
+                {/* Section ID */}
+                <InputFormField
+                  disabled={true}
+                  form={editSectionForm}
+                  title='Section ID'
+                  placeholder='CETBSCS0401'
+                  fieldName='sectionId'
+                  badge={<Badge variant='outline'>Auto-generated</Badge>}
+                />
+
+                {/* Department */}
+                <SelectFormField
+                  form={editSectionForm}
+                  content={collegeDepartments}
+                  title='Department'
+                  placeholder='Select department...'
+                  fieldName='collegeDepartment'
+                />
+
+                {/* Programs */}
+                <SelectFormField
+                  form={editSectionForm}
+                  content={collegePrograms}
+                  title='Program'
+                  placeholder='Select program...'
+                  fieldName='collegeProgram'
+                />
+
+                {/* Year Level and Section */}
+                <section className='w-full flex gap-2 justify-items-start'>
+                  {/* Year Level */}
+                  <SelectFormField
+                    form={editSectionForm}
+                    content={collegeYear}
+                    title='Year Level'
+                    placeholder='Select year level...'
+                    fieldName='collegeYear'
+                  />
+                  <Label className='flex items-center'>-</Label>
+                  {/* Section */}
+                  <SelectFormField
+                    form={editSectionForm}
+                    content={collegeSection}
+                    title='Section'
+                    placeholder='Select section...'
+                    fieldName='collegeSection'
+                  />
+                </section>
+
+                {/* Checkbox for Confirmation */}
+                <div className='items-top flex space-x-2 pt-2'>
+                  <Checkbox id='confirm' />
+                  <div className='grid gap-1.5 leading-none'>
+                    <label
+                      htmlFor='confirm'
+                      className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    >
+                      Are you sure?
+                    </label>
+                    <p className='text-sm text-muted-foreground'>
+                      Double check if all inputs are correct to make sure there
+                      are no input errors.
+                    </p>
+                  </div>
+                </div>
+
+                <DialogFooter className='w-full flex justify-end mt-4'>
+                  <DialogClose asChild>
+                    <Button variant='outline'>Cancel</Button>
+                  </DialogClose>
+                  <Button type='submit'>Save Section</Button>
+                </DialogFooter>
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  function AddSectionUndergrad() {
+    const addSectionForm = useForm({
+      resolver: zodResolver(UserSchema),
+      defaultValues: {
+        userSchemaDefaultValues,
+      },
+    });
+
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Add Section</Button>
+        </DialogTrigger>
+        <DialogContent className='sm:max-w-[500px]'>
+          <DialogHeader>
+            <DialogTitle>Add Section</DialogTitle>
+          </DialogHeader>
+
+          <Form {...addSectionForm}>
+            <form onSubmit={addSectionForm.handleSubmit(onSubmit)}>
+              {/* Content */}
+              <div className='flex flex-col gap-2'>
+                {/* Section ID */}
+                <InputFormField
+                  disabled={true}
+                  form={addSectionForm}
+                  title='Section ID'
+                  placeholder='CETBSCS0401'
+                  fieldName='sectionId'
+                  badge={<Badge variant='outline'>Auto-generated</Badge>}
+                />
+
+                {/* Year Level and Section */}
+                <section className='w-full flex gap-2 justify-items-start'>
+                  {/* Year Level */}
+                  <SelectFormField
+                    form={addSectionForm}
+                    content={collegeYear}
+                    title='Year Level'
+                    placeholder='Select year level...'
+                    fieldName='collegeYear'
+                  />
+                  <Label className='flex items-center'>-</Label>
+                  {/* Section */}
+                  <SelectFormField
+                    form={addSectionForm}
+                    content={collegeSection}
+                    title='Section'
+                    placeholder='Select section...'
+                    fieldName='collegeSection'
+                  />
+                </section>
+
+                {/* Checkbox for Confirmation */}
+                <div className='items-top flex space-x-2 pt-2'>
+                  <Checkbox id='confirm' />
+                  <div className='grid gap-1.5 leading-none'>
+                    <label
+                      htmlFor='confirm'
+                      className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+                    >
+                      Are you sure?
+                    </label>
+                    <p className='text-sm text-muted-foreground'>
+                      Double check if all inputs are correct to make sure there
+                      are no input errors.
+                    </p>
+                  </div>
+                </div>
+
+                <DialogFooter className='w-full flex justify-end mt-4'>
+                  <Button type='submit'>Save Section</Button>
+                </DialogFooter>
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    );
+  }
 
   const fakeSectionTemplate = [
     {
@@ -372,6 +371,7 @@ function CollegeSectionsPage() {
             />
             <EditSectionUndergrad
               disabled={Object.keys(selectedSection).length === 0}
+              onClick={() => console.log('edit')}
             />
             <AddSectionUndergrad />
           </div>
