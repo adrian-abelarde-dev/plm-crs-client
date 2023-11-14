119740b3a517e816fad59365e890257071c5a9d2
JamelSusi25 <jesusi2020@plm.edu.ph>
2023-11-14 13:57:11 +0800

    [Refactor][Jamel] Change students information card

--- src/app/(private-routes)/student/home/page.js
+++ src/app/(private-routes)/student/home/page.js
@@ -1,7 +1,6 @@
 'use client';
-
-import { AlertTitle } from '@/components/ui/alert';
 import { Badge } from '@/components/ui/badge';
+import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import {
   Table,
@@ -12,20 +11,36 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { fakeStudentsDashboard } from '@/lib/constants/fake-data/schedule';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { IconBrandTeams } from '@tabler/icons-react';
 import {
   AlertCircle,
   Briefcase,
-  CalendarIcon,
+  CalendarCheck,
   Printer,
   School,
 } from 'lucide-react';
 import Link from 'next/link';
 
+
 function StudentHomePage() {
+  const program = 'BS Computer Science';
+  const college = 'College of Engineering and Technology - Graduate Program';
+  const schoolYear = 'School Year 2023 - 2024 1st Semester';
+
   return (
-    <main className='w-full p-20'>
-      <h1 className='font-bold text-2xl py-12'>Student Dashboard</h1>
+    <div className='ml-9 mr-9'>
+    {/* Title - Student Dashboard */}
+    <div className='mt-32'>
+      <Label className='font-[500] text-4xl '>Student Dashboard</Label>
+    </div>
       <div className='flex space-x-3'>
         <div className='rounded-md border w-3/4 p-2'>
           {/*Table for Schedules*/}
@@ -52,55 +67,60 @@ function StudentHomePage() {
           </Table>
         </div>
         {/*Table for Student Information*/}
-        <div className='rounded-md border w-96 p-4'>
-          <Badge variant='outlineGreen' className='mb-8'>
-            Regular
-          </Badge>
-          <h1 className='font-bold text-lg'>Juan Dela Cruz</h1>
-          <div className='flex gap-2'>
-            <AlertCircle className='h-4 w-4' />
-            <AlertTitle>2020-11299</AlertTitle>
-          </div>
-          <h1 className='py-4'></h1>
-          <div className='flex gap-1'>
-            <Briefcase className='h-4 w-4 mr-2' />
-            <h1>Computer Science</h1>
-          </div>
-          <div className='flex gap-2'>
-            <School className='h-6 w-6 mr-1.5' />
-            <h1>College of Engineering and Technology - Graduate Program</h1>
-          </div>
-          <div className='flex gap-1'>
-            <CalendarIcon className='h-4 w-4 mr-2' />
-            <h1 className=''>School Year 2023 - 2024 1st Semester</h1>
-          </div>
-          <div className='py-5'>
-            <div className='flex space-x-3'>
-              <Button
-                variant='outline'
-                className='bg-yellow-400 hover:bg-yellow-500'
-              >
-                <Printer className='h-4 w-4 mr-2' />
-                Print SER
-              </Button>
-              <Button
-                variant='outline'
-                className='bg-[#5458AE] hover:bg-[#2e3281] hover:text-white md:max-xl:mt-4 text-white'
-              >
-                <IconBrandTeams className='h-4 w-4 mr-2' />
-                <Link
-                  href='https://www.youtube.com/watch?v=oHPjYCS3JKQ'
-                  target='_blank'
+        <Card className='w-1/3 max-md:w-full'>
+        <CardHeader>
+            <CardTitle>
+              <div className=''>
+                <Badge
+                  variant='outline'
+                  className='bg-[#FEF2F2] text-[#DC2626] border-[#DC2626]'
                 >
-                  {' '}
-                  Open Teams
-                </Link>
-              </Button>
-            </div>
-          </div>
-        </div>
+                  Not Enrolled
+                </Badge>
+                <div className='mt-5'>
+                  <Label className='font-[600] text-xl '>
+                    Juan Dela Cruz
+                  </Label>
+                </div>
+              </div>
+            </CardTitle>
+            <CardDescription className='flex flex-row '>
+            <AlertCircle className='h-5 w-5 pt-1 -mt-2.5' />
+              <Label className='-mt-1'>2020-110299</Label>
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='mt-12'>
+            <Label className='text-base flex flex-row'>
+              <Briefcase className='h-4 w-4 mr-2 mt-1' />
+              {program}
+            </Label>
+            <Label className='text-base flex flex-row'>
+              <School className='h-4 w-4 mr-2 mt-1' />
+              {college}
+            </Label>
+            <Label className='text-base flex flex-row'>
+              <CalendarCheck className='h-4 w-4 mr-2 mt-1' />
+              {schoolYear}
+            </Label>
+          </CardContent>
+          <CardFooter className='mt-12 md:max-xl:block'>
+            <Button className='bg-yellow-400 text-[#0F172A] mr-[0.62rem] hover:bg-yellow-500'>
+              <Printer className='h-4 w-4 mr-2' /> Print SER
+            </Button>
+            <Button className='bg-[#5458AE] hover:bg-[#2e3281] md:max-xl:mt-4 text-white'>
+              <IconBrandTeams className='h-4 w-4 mr-2' />
+              <Link
+                href='https://www.youtube.com/watch?v=MdLbC-MmNac'
+                target='_blank'
+              >
+                Open Teams
+              </Link>
+            </Button>
+          </CardFooter>
+
+        </Card>
       </div>
-    </main>
+    </div>
   );
 }
 
