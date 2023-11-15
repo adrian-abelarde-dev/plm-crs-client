715d90b112958c681b509313496de1ed23202bee
urquico <kurtjacob.urquico@gmail.com>
2023-11-15 17:24:36 +0800

    [Feature][Kurt] - Added table

--- src/app/(private-routes)/college-grad/transactions/teaching-assignment/page.jsx
+++ src/app/(private-routes)/college-grad/transactions/teaching-assignment/page.jsx
@@ -1,7 +1,144 @@
-import React from 'react';
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { facultyTeachingAssignments } from '@/lib/constants/fake-data/teaching-assignments-grad';
+import { useState } from 'react';
 
 function CollegeGradTeachingAssignment() {
-  return <div>CollegeGradTeachingAssignment</div>;
+  const [facultyInput, setFacultyInput] = useState('Urquico, Kurt Jacob E.');
+
+  // compute for total units
+  function totalUnits(key) {
+    return facultyTeachingAssignments.reduce(
+      (total, subject) => total + subject[key],
+      0,
+    );
+  }
+
+  return (
+    <main className='p-6'>
+      <div className='mt-12'>
+        <Label className='text-4xl font-medium'>Teaching Assignment</Label>
+      </div>
+
+      <div className='w-[20rem] mb-10 mt-16'>
+        <Label htmlFor='email'>Search Faculty</Label>
+        <Input
+          id='faculty'
+          placeholder='Search'
+          onChange={(e) => setFacultyInput(e.target.value)}
+          value={facultyInput}
+        />
+      </div>
+
+      {facultyInput !== '' ? (
+        <>
+          {/* Faculty Name */}
+          <div className='flex flex-col place-items-start mb-16'>
+            <Label className='text-4xl uppercase font-bold'>
+              {facultyInput}
+            </Label>
+            <Label>Lecturer II, Part Time</Label>
+          </div>
+
+          <div className='flex flex-col place-items-center'>
+            <Label className='text-lg uppercase font-medium'>
+              College of Engineering and Technology
+            </Label>
+            <Label>1st Sem, SY 2023-2024</Label>
+          </div>
+          <Table className='mt-8'>
+            <TableCaption>
+              This college has considered you to teach the following subject(s)
+              for the stipulated term.
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className='font-bold text-black'>
+                  Subject Code
+                </TableHead>
+                <TableHead className='font-bold text-black'>Section</TableHead>
+                <TableHead className='font-bold text-black'>
+                  Subject Title
+                </TableHead>
+                <TableHead className='font-bold text-black text-center'>
+                  Units
+                </TableHead>
+                <TableHead className='font-bold text-black'>Schedule</TableHead>
+                <TableHead className='font-bold text-black'>
+                  No. of Students
+                </TableHead>
+                <TableHead className='font-bold text-black text-center'>
+                  Credited Units
+                </TableHead>
+                <TableHead className='font-bold text-black'>College</TableHead>
+                <TableHead className='font-bold text-black'>
+                  Type of Load
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {facultyTeachingAssignments.map((subjects, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell>{subjects.subjectCode}</TableCell>
+                    <TableCell>{subjects.section}</TableCell>
+                    <TableCell>{subjects.subjectTitle}</TableCell>
+                    <TableCell className='text-center'>
+                      {subjects.units.toFixed(1)}
+                    </TableCell>
+                    <TableCell>{subjects.schedule}</TableCell>
+                    <TableCell>{subjects.noOfStudents}</TableCell>
+                    <TableCell className='text-center'>
+                      {subjects.creditedUnits.toFixed(1)}
+                    </TableCell>
+                    <TableCell>{subjects.college}</TableCell>
+                    <TableCell>{subjects.typeOfLoad}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell className='text-center'>
+                  Total No. of Units:{' '}
+                  <span className='text-bold'>{totalUnits('units')}</span>
+                </TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+                <TableCell className='text-center'>
+                  Total No. of Credits:{' '}
+                  <span className='text-bold'>
+                    {totalUnits('creditedUnits')}
+                  </span>
+                </TableCell>
+                <TableCell></TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </>
+      ) : (
+        <div className='w-full h-96 flex justify-center place-items-center text-zinc-400 '>
+          Please search for a Curriculum
+        </div>
+      )}
+    </main>
+  );
 }
 
 export default CollegeGradTeachingAssignment;
