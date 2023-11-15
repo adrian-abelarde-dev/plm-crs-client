2d9aeec57363ecdbd1af8d19fd0ea7eb0449c46d
urquico <44643247+urquico@users.noreply.github.com>
2023-11-15 15:18:15 +0800

    [Feature][Kurt] - Added College Grad Curriculum Management (#70)
    * [Feature][Kurt] - Added tabs
    
    * [Feature][Kurt] - Added table that displays when input exists
    
    * [Feature][Kurt] - Handles delete curriculum
    
    * [Feature][Kurt] - Added Add course modal
    
    * [Feature][Kurt] - Added Affected Programs
    
    * [Refactor][Kurt] - Refactored Dialog into AlertDialog

--- src/app/(private-routes)/college-grad/management/curriculum/page.jsx
+++ src/app/(private-routes)/college-grad/management/curriculum/page.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { Label } from '@/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import AffectedProgram from './affected-program';
+import ListOfCurriculums from './list-of-curriculum';
 
 function CollageGradManagementCurriculum() {
-  return <div>CollageGradManagementCurriculum</div>;
+  return (
+    <main className='p-6'>
+      <Tabs defaultValue='listOfCurriculums' className='w-full mt-12'>
+        <div className='flex justify-between mb-4 max-md:block'>
+          <Label className='text-4xl font-medium'>Curriculum Management</Label>
+          <TabsList>
+            <TabsTrigger value='listOfCurriculums' className='px-10'>
+              List of Curriculums
+            </TabsTrigger>
+            <TabsTrigger value='affectedPrograms' className='px-10'>
+              Affected Programs
+            </TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value='listOfCurriculums' className='w-full'>
+          <ListOfCurriculums />
+        </TabsContent>
+        <TabsContent value='affectedPrograms'>
+          <AffectedProgram />
+        </TabsContent>
+      </Tabs>
+    </main>
+  );
 }
 
 export default CollageGradManagementCurriculum;
