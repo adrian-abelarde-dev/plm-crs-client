8c372e3ce32688a5b01194eca9eb13debf5ecc9e
AC Salazar <accsalazar2020@plm.edu.ph>
2023-11-17 21:18:45 +0800

    [Feature][Althea] Separated codes into diff. components

--- src/components/component/college/college-schedule-add-class.jsx
+++ src/components/component/college/college-schedule-add-class.jsx
@@ -36,6 +36,11 @@ import {
   UserSchema,
   userSchemaDefaultValues,
 } from '@/lib/constants/schema/user';
+import {
+  fakeCollegeClassHoursTemplate,
+  fakeCollegeClassResTemplate,
+  fakeCollegeFacultyTemplate,
+} from '@/lib/constants/table-templates/college/college-schedule-table';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CheckCircle, Delete, Edit, HelpCircle } from 'lucide-react';
 import * as React from 'react';
@@ -135,81 +140,6 @@ function AddClassUndergrad() {
       </Button>
     );
   }
-
-  const fakeCollegeFacultyTemplate = [
-    {
-      accessorKey: 'facultyFName',
-      id: 'facultyFName',
-      header: 'First Name',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'facultyMName',
-      id: 'facultyMName',
-      header: 'Middle Name',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'facultyLName',
-      id: 'facultyLName',
-      header: 'Last Name',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'facultyEmail',
-      id: 'facultyEmail',
-      header: 'PLM Email',
-      filterVariant: 'fuzzy',
-    },
-  ];
-
-  const fakeCollegeClassHoursTemplate = [
-    {
-      accessorKey: 'day',
-      id: 'day',
-      header: 'Day',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'timeStart',
-      id: 'timeStart',
-      header: 'Start Time',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'timeEnd',
-      id: 'timeEnd',
-      header: 'End Time',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'room',
-      id: 'room',
-      header: 'Room',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'meetingType',
-      id: 'meetingType',
-      header: 'Meeting Type',
-      filterVariant: 'fuzzy',
-    },
-  ];
-
-  const fakeCollegeClassResTemplate = [
-    {
-      accessorKey: 'scope',
-      id: 'scope',
-      header: 'Scope',
-      filterVariant: 'fuzzy',
-    },
-    {
-      accessorKey: 'restriction',
-      id: 'restriction',
-      header: 'Restriction',
-      filterVariant: 'fuzzy',
-    },
-  ];
 
   return (
     <Dialog>
--- src/lib/constants/table-templates/college/college-schedule-table.js
+++ src/lib/constants/table-templates/college/college-schedule-table.js
@@ -24,3 +24,78 @@ export const fakeCollegeScheduleTemplate = [
     filterVariant: 'fuzzy',
   },
 ];
+
+export const fakeCollegeFacultyTemplate = [
+  {
+    accessorKey: 'facultyFName',
+    id: 'facultyFName',
+    header: 'First Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'facultyMName',
+    id: 'facultyMName',
+    header: 'Middle Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'facultyLName',
+    id: 'facultyLName',
+    header: 'Last Name',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'facultyEmail',
+    id: 'facultyEmail',
+    header: 'PLM Email',
+    filterVariant: 'fuzzy',
+  },
+];
+
+export const fakeCollegeClassHoursTemplate = [
+  {
+    accessorKey: 'day',
+    id: 'day',
+    header: 'Day',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'timeStart',
+    id: 'timeStart',
+    header: 'Start Time',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'timeEnd',
+    id: 'timeEnd',
+    header: 'End Time',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'room',
+    id: 'room',
+    header: 'Room',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'meetingType',
+    id: 'meetingType',
+    header: 'Meeting Type',
+    filterVariant: 'fuzzy',
+  },
+];
+
+export const fakeCollegeClassResTemplate = [
+  {
+    accessorKey: 'scope',
+    id: 'scope',
+    header: 'Scope',
+    filterVariant: 'fuzzy',
+  },
+  {
+    accessorKey: 'restriction',
+    id: 'restriction',
+    header: 'Restriction',
+    filterVariant: 'fuzzy',
+  },
+];
