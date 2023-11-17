c8eaef2f73f6d195310978dfc5b377c6b333360f
adrian-abelarde-dev <150082811+adrian-abelarde-dev@users.noreply.github.com>
2023-11-17 20:00:48 +0800

    [Fix][Adrian] Add consistent student and student-grad home (#90)
    Co-authored-by: drianlarde <aadabelarde2020@plm.edu.ph>

--- src/app/(private-routes)/student-grad/home/page.jsx
+++ src/app/(private-routes)/student-grad/home/page.jsx
@@ -1,5 +1,5 @@
 import TableMRT from '@/components/layouts/table-mrt';
-import { Alert, AlertTitle } from '@/components/ui/alert';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
@@ -10,15 +10,8 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableRow,
-} from '@/components/ui/table';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 import { fakeGradAssessmentHistories } from '@/lib/constants/fake-data/grad-assessment-history';
 import { fakeGradClasses } from '@/lib/constants/fake-data/grad-classes';
 import { fakeGradPaymentHistories } from '@/lib/constants/fake-data/grad-payment-history';
@@ -38,60 +31,53 @@ import {
 } from 'lucide-react';
 import Link from 'next/link';
 
+const program = '(MIT) Master of Information Technology';
+const college = 'CET - Graduate Program';
+const schoolYear = 'School Year 2023 - 2024 1st Semester';
+const alertMessage =
+  'You are not yet officially enrolled for this current term. Please settle your dues.';
+
 function GradStudentHome() {
-  const program = '(MIT) Master of Information Technology';
-  const college = 'CET - Graduate Program';
-  const schoolYear = 'School Year 2023 - 2024 1st Semester';
-
-  const alertMessage =
-    'You are not yet officially enrolled for this current term. Please settle your dues.';
-
   return (
     <div className='ml-9 mr-9'>
       {/* Title - Student Dashboard */}
-      <div className='mt-32'>
-        <Label className='font-[500] text-4xl '>Student Dashboard</Label>
-      </div>
+      <h1 className='mt-32 font-medium text-4xl'>Student Dashboard</h1>
 
       {/* Student Dashboard Content */}
       <div className='mt-10 flex flex-col md:flex-row gap-4'>
         {/* Student Info */}
-        <Card className='w-1/3 max-md:w-full'>
+        <Card className='w-1/3 max-md:w-full flex flex-col justify-between'>
           <CardHeader>
-            <CardTitle>
-              <div className=''>
-                <Badge
-                  variant='outline'
-                  className='bg-[#FEF2F2] text-[#DC2626] border-[#DC2626]'
-                >
-                  Not Enrolled
-                </Badge>
-                <div className='mt-5'>
-                  <Label className='font-[600] text-xl '>
-                    Kurt Jacob Urquico
-                  </Label>
-                </div>
-              </div>
+            <Badge
+              variant='outline'
+              className='bg-destructive/10 text-destructive border-destructive w-fit'
+            >
+              Not Enrolled
+            </Badge>
+            <CardTitle className='font-semibold text-2xl mt-4'>
+              Kurt Jacob Urquico
             </CardTitle>
-            <CardDescription className='flex flex-row '>
-              <Info className='h-4 w-4 pt-1 -mt-2' />
-              <Label className='-mt-1'>2020-42069</Label>
+            <CardDescription className='flex flex-row gap-2 items-center'>
+              <Info className='h-auto w-4 pt-1 -mt-2' />
+              2020-42069
             </CardDescription>
           </CardHeader>
-          <CardContent className='mt-12'>
-            <Label className='text-base flex flex-row'>
+
+          <CardContent>
+            <h1 className='text-base flex flex-row'>
               <Briefcase className='h-4 w-4 mr-2 mt-1' />
               {program}
-            </Label>
-            <Label className='text-base flex flex-row'>
+            </h1>
+            <h1 className='text-base flex flex-row'>
               <School className='h-4 w-4 mr-2 mt-1' />
               {college}
-            </Label>
-            <Label className='text-base flex flex-row'>
+            </h1>
+            <h1 className='text-base flex flex-row'>
               <CalendarCheck className='h-4 w-4 mr-2 mt-1' />
               {schoolYear}
-            </Label>
+            </h1>
           </CardContent>
+
           <CardFooter className='mt-12 md:max-xl:block'>
             <Button className='bg-yellow-400 text-[#0F172A] mr-[0.62rem] hover:bg-yellow-500'>
               <Printer className='h-4 w-4 mr-2' /> Print SER
@@ -111,17 +97,16 @@ function GradStudentHome() {
         {/* Payment */}
         <div className='md:max-xl:ml-4 w-2/3 max-md:w-full flex flex-col justify-between max-md:mt-4'>
           {/* Alert */}
-          <Alert className='bg-[#fefce8] max-md:mb-4'>
-            <AlertTriangle className='h-4 w-4 -mt-1 text-yellow-700' />
-            <AlertTitle className='text-yellow-700'>{alertMessage}</AlertTitle>
+          <Alert className='border-yellow-700 border-2 bg-yellow/20 text-yellow-700'>
+            <AlertTriangle className='w-4 h-4 -mt-1' color='#A16207' />
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>{alertMessage} </AlertDescription>
           </Alert>
 
           {/* Paayment Table */}
           <Card>
-            <CardHeader>
-              <TableCaption className='font-bold'>
-                Payment for Academic Year 2023 - 2024 1st Trimester
-              </TableCaption>
+            <CardHeader className='flex justify-center items-center font-semibold'>
+              Payment for Academic Year 2023 - 2024 1st Trimester
             </CardHeader>
 
             <Separator className='mb-4' />
@@ -131,7 +116,7 @@ function GradStudentHome() {
                 <TableBody>
                   <TableRow>
                     <TableCell>Pay Type/Term</TableCell>
-                    <TableCell className='font-bold'>
+                    <TableCell className='font-medium'>
                       Five Partial Payment
                     </TableCell>
                   </TableRow>
@@ -141,17 +126,17 @@ function GradStudentHome() {
                       Total Amount{' '}
                       <span className='text-zinc-500'>(Current Term)</span>
                     </TableCell>
-                    <TableCell className='font-bold'>₱ 6,666.00</TableCell>
+                    <TableCell className='font-medium'>₱ 6,666.00</TableCell>
                   </TableRow>
 
                   <TableRow>
                     <TableCell>Amount to be paid for 2nd payment</TableCell>
-                    <TableCell className='font-bold'>₱ 6,666.00</TableCell>
+                    <TableCell className='font-medium'>₱ 6,666.00</TableCell>
                   </TableRow>
 
                   <TableRow>
                     <TableCell>Overall Balance</TableCell>
-                    <TableCell className='font-bold'>₱ 9,999.00</TableCell>
+                    <TableCell className='font-medium'>₱ 9,999.00</TableCell>
                   </TableRow>
                 </TableBody>
               </Table>
@@ -186,9 +171,6 @@ function GradStudentHome() {
           data={fakeGradClasses}
           title={'Classes'}
           searchPlaceholder={'Search class'}
-          RightButtons={<></>}
-          LeftButtons={<></>}
-          RowActions={<></>}
         />
       </div>
 
@@ -199,9 +181,6 @@ function GradStudentHome() {
           data={fakeGradPaymentHistories}
           title={'Payment History'}
           searchPlaceholder={'Search payment history'}
-          RightButtons={<></>}
-          LeftButtons={<></>}
-          RowActions={<></>}
           isRowNumbersVisible={true}
         />
       </div>
@@ -213,9 +192,6 @@ function GradStudentHome() {
           data={fakeGradAssessmentHistories}
           title={'Assessment History'}
           searchPlaceholder={'Search assessment history'}
-          RightButtons={<></>}
-          LeftButtons={<></>}
-          RowActions={<></>}
           isRowNumbersVisible={true}
         />
       </div>
