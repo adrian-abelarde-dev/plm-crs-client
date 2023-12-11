c326b851e1c1e0b5670144465939ff31f74f47eb
urquico <kurtjacob.urquico@gmail.com>
2023-12-11 16:00:31 +0800

    [Feature][Kurt] - Adding layout for encoding grades for students

--- src/app/(private-routes)/faculty/encoding-grades/[courseCode]/page.jsx
+++ src/app/(private-routes)/faculty/encoding-grades/[courseCode]/page.jsx
@@ -1,7 +1,18 @@
+import { Label } from '@/components/ui/label';
 import React from 'react';
 
 function GradesCourseView({ params }) {
-  return <div>{params.courseCode}</div>;
+  return (
+    <main className='p-6'>
+      <div className='mt-12'>
+        <Label className='text-4xl font-medium'>Encoding of Grades</Label>
+      </div>
+
+      <div className='mt-8 flex flex-col items-center'>
+        <Label className='text-lg font-semibold'>REPORT OF GRADES</Label>
+      </div>
+    </main>
+  );
 }
 
 export default GradesCourseView;
