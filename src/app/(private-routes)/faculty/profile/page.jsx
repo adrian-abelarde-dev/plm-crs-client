e8f6416c96f98af5565336762947c6af65a8d80f
urquico <kurtjacob.urquico@gmail.com>
2023-12-17 15:12:32 +0800

    [Feature][Kurt] - Added information layout

--- src/app/(private-routes)/faculty/profile/page.jsx
+++ src/app/(private-routes)/faculty/profile/page.jsx
@@ -2,10 +2,131 @@
 
 import { useState } from 'react';
 
-import PersonalDetails from './personal-details';
+import DetailsSection from './details-section';
 
 function FacultyProfile() {
   const [contactNo, setContactNo] = useState('09876543211');
+  const [address, setAddress] = useState('1234 Bambang St. Sta. Cruz, Manila');
+
+  const personalData = {
+    title: 'Personal Details',
+    content: [
+      {
+        cell: [
+          {
+            label: 'Employee No.',
+            value: '12345678910',
+          },
+          {
+            label: 'Last Name',
+            value: 'Dela Santos',
+          },
+          {
+            label: 'First Name',
+            value: 'John Michael',
+          },
+          {
+            label: 'Middle Name',
+            value: 'Delimondo',
+          },
+          {
+            label: 'Maiden Name',
+            value: '',
+          },
+          {
+            label: 'Pedigree',
+            value: 'Doc',
+          },
+        ],
+      },
+      {
+        cell: [
+          {
+            label: 'Birth Date',
+            value: '12/25/1987',
+          },
+          {
+            label: 'Birth Place',
+            value: 'Manila City',
+          },
+          {
+            label: 'Email Address',
+            value: 'gmailkoto@gmail.com',
+          },
+        ],
+      },
+      {
+        cell: [
+          {
+            label: 'Gender',
+            value: 'Male',
+          },
+          {
+            label: 'Status',
+            value: 'Single',
+          },
+          {
+            label: 'Citizenship',
+            value: 'Filipino',
+          },
+          {
+            label: 'Contact Number',
+            value: contactNo,
+            editable: true,
+            onChange: (e) => setContactNo(e.target.value),
+            type: 'number',
+          },
+        ],
+      },
+    ],
+  };
+
+  const employmentDetails = {
+    title: 'Employment Details',
+    content: [
+      {
+        cell: [
+          { label: 'TIN No.', value: '12345678910' },
+          { label: 'GSIS No.', value: '12345678910' },
+          { label: 'Instructor Code', value: 'JMDelaSantos' },
+        ],
+      },
+    ],
+  };
+
+  const currentAddress = {
+    title: 'Current Address',
+    content: [
+      {
+        cell: [
+          {
+            label: 'Street Address',
+            value: address,
+            editable: true,
+            onChange: (e) => setAddress(e.target.value),
+          },
+          {
+            label: 'Province City',
+            value: 'Cabanatuan City',
+          },
+        ],
+      },
+      {
+        cell: [
+          {
+            label: 'Zip Code',
+            value: '1024',
+          },
+          {
+            label: 'Phone Number',
+            value: '(02) 1-2345678',
+          },
+        ],
+      },
+    ],
+  };
+
+  console.log(address);
 
   return (
     <main className='p-6'>
@@ -15,13 +136,14 @@ function FacultyProfile() {
 
       {/* Profile */}
       <div className='flex flex-col shadow border h-auto mt-4 rounded-sm p-6'>
-        <PersonalDetails contactNo={contactNo} setContactNo={setContactNo} />
+        {/* Personal Details */}
+        <DetailsSection data={personalData} setContactNo={setContactNo} />
 
         {/* Employment Details */}
-        <div></div>
+        <DetailsSection data={employmentDetails} setContactNo={setContactNo} />
 
         {/* Current Address */}
-        <div></div>
+        <DetailsSection data={currentAddress} setContactNo={setContactNo} />
       </div>
     </main>
   );
