b1185b4aeb186750c61512796e9e6accfdc2d019
JamelSusi25 <jesusi2020@plm.edu.ph>
2023-12-26 18:42:33 +0800

    [Refactor][Jamel] - Improve code readability in user service

--- src/app/(private-routes)/student/information/page.js
+++ src/app/(private-routes)/student/information/page.js
@@ -57,16 +57,16 @@ function StudentInformationPage() {
   const [middleName, setMiddleName] = useState('');
   const [lastName, setLastName] = useState('');
   const [pedigree, setPedigree] = useState('');
-  const [Gender, setGender] = useState('');
+  const [gender, setGender] = useState('');
   const [civilStatus, setCivilStatus] = useState('');
-  const [CountryofCitizen, setCountryoCitizen] = useState('');
+  const [countryofcitizen, setCountryoCitizen] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [studentType, setStudentType] = useState('');
   const [studentStatus, setStudentStatus] = useState('');
   const [studentDegree, setStudentDegree] = useState('');
   const [yearLevel, setYearLevel] = useState('');
-  const [PLMemail, setPLMEmail] = useState('');
-  const [personalEmail, setPersonalEmail] = useState('');
+  const [plmemail, setPLMEmail] = useState('');
+  const [personalemail, setPersonalEmail] = useState('');
   // ... add another fields
   const civilStatusOptions = [
     'Single',
@@ -77,7 +77,7 @@ function StudentInformationPage() {
   ];
   const status = ['new', 'old'];
   const regstatus = ['regular', 'irregular'];
-  const Degree = [
+  const degree = [
     'Bachelor of Science in Information Technology',
     'Bachelor of Science in Computer Science',
     'Bachelor of Science Information System',
@@ -89,153 +89,157 @@ function StudentInformationPage() {
   };
 
   return (
-    <div className='flex flex-col mt-4'>
-      <h1 className='mt-32 font-medium text-3xl'>Information</h1>
-      <h1 className='mt-10 font-medium text-2xl'>Student Profile</h1>
+    <div className='container mx-auto mt-8 px-4 lg:px-0'>
+      <h1 className='mb-4 mt-32 font-medium text-3xl'>Information</h1>
+      <h1 className='mb-4 mt-10 font-medium text-2xl'>Student Profile</h1>
       <div className='container mx-auto mt-8'>
-        <form>
-          <div className='w-1/4'>
-            <InputField
-              label='Student ID'
-              value={studentId}
-              onChange={(e) => setStudentId(e.target.value)}
-              disabled={!isFormEnabled}
-            />
-          </div>
-          <div className='flex justify-between gap-4'>
-            <div className='w-1/3'>
+        <form className='relative'>
+          <div className='flex flex-wrap mb-4 -mx-1'>
+            <div className='w-1/3 md:w-768px sm:w-640px'>
               <InputField
-                label='First Name'
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                label='Student ID'
+                value={studentId}
+                onChange={(e) => setStudentId(e.target.value)}
                 disabled={!isFormEnabled}
               />
             </div>
-            <div className='w-1/3'>
-              <InputField
-                label='Middle Name'
-                value={middleName}
-                onChange={(e) => setMiddleName(e.target.value)}
-                disabled={!isFormEnabled}
-              />
+            <div className='flex justify-between gap-5'>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='First Name'
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Middle Name'
+                  value={middleName}
+                  onChange={(e) => setMiddleName(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Last Name'
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
             </div>
-            <div className='w-1/3'>
-              <InputField
-                label='Last Name'
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-          </div>
-          <div className='flex justify-between gap-4'>
-            <div className='w-1/4'>
-              <InputField
-                label='Pedigree'
-                value={pedigree}
-                onChange={(e) => setPedigree(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-            <div className='w-1/4'>
-              <InputField
-                label='Gender'
-                value={Gender}
-                onChange={(e) => setGender(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-            <div className='w-1/4'>
-              <InputField
-                label='Civil Status'
-                value={civilStatus}
-                onChange={(e) => setCivilStatus(e.target.value)}
-                disabled={!isFormEnabled}
-                isDropdown={true}
-                options={civilStatusOptions}
-              />
-            </div>
-            <div className='w-1/4'>
-              <InputField
-                label='Country of Citizenship'
-                value={CountryofCitizen}
-                onChange={(e) => setCountryoCitizen(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-            <div className='w-1/4'>
+            <div className='flex justify-between gap-5'>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Pedigree'
+                  value={pedigree}
+                  onChange={(e) => setPedigree(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Gender'
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Civil Status'
+                  value={civilStatus}
+                  onChange={(e) => setCivilStatus(e.target.value)}
+                  disabled={!isFormEnabled}
+                  isDropdown={true}
+                  options={civilStatusOptions}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Country of Citizenship'
+                  value={countryofcitizen}
+                  onChange={(e) => setCountryoCitizen(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
               <div className=''>
-                <label className='block text-sm font-bold mb-1'>
-                  Phone Number
-                </label>
-                <PhoneInput
-                  country={'ph'} // default country
-                  value={phoneNumber}
-                  onChange={setPhoneNumber}
+                <div className='w-full md:w-768px sm:w-640px'>
+                  <label className='block text-sm font-bold mb-1'>
+                    Phone Number
+                  </label>
+                  <PhoneInput
+                    className='flex justify-between'
+                    country={'ph'} // default country
+                    value={phoneNumber}
+                    onChange={setPhoneNumber}
+                    disabled={!isFormEnabled}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='flex justify-between gap-4'>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Student Type'
+                  value={studentType}
+                  onChange={(e) => setStudentType(e.target.value)}
+                  disabled={!isFormEnabled}
+                  isDropdown={true}
+                  options={status}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Registration Status'
+                  value={studentStatus}
+                  onChange={(e) => setStudentStatus(e.target.value)}
+                  disabled={!isFormEnabled}
+                  isDropdown={true}
+                  options={regstatus}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Degree Program'
+                  value={studentDegree}
+                  onChange={(e) => setStudentDegree(e.target.value)}
+                  disabled={!isFormEnabled}
+                  isDropdown={true}
+                  options={degree}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Year Level'
+                  value={yearLevel}
+                  onChange={(e) => setYearLevel(e.target.value)}
+                  disabled={!isFormEnabled}
+                  isDropdown={true}
+                  options={yearlvl}
+                />
+              </div>
+            </div>
+            <div className='flex justify-between gap-4'>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Official PLM Email'
+                  value={plmemail}
+                  onChange={(e) => setPLMEmail(e.target.value)}
+                  disabled={!isFormEnabled}
+                />
+              </div>
+              <div className='w-full md:w-768px sm:w-640px'>
+                <InputField
+                  label='Personal Email'
+                  value={personalemail}
+                  onChange={(e) => setPersonalEmail(e.target.value)}
                   disabled={!isFormEnabled}
                 />
               </div>
             </div>
           </div>
-          <div className='flex justify-between gap-4'>
-            <div className='w-1/4'>
-              <InputField
-                label='Student Type'
-                value={studentType}
-                onChange={(e) => setStudentType(e.target.value)}
-                disabled={!isFormEnabled}
-                isDropdown={true}
-                options={status}
-              />
-            </div>
-            <div className='w-1/4'>
-              <InputField
-                label='Registration Status'
-                value={studentStatus}
-                onChange={(e) => setStudentStatus(e.target.value)}
-                disabled={!isFormEnabled}
-                isDropdown={true}
-                options={regstatus}
-              />
-            </div>
-            <InputField
-              label='Degree Program'
-              value={studentDegree}
-              onChange={(e) => setStudentDegree(e.target.value)}
-              disabled={!isFormEnabled}
-              isDropdown={true}
-              options={Degree}
-            />
-            <div className='w-1/4'>
-              <InputField
-                label='Year Level'
-                value={yearLevel}
-                onChange={(e) => setYearLevel(e.target.value)}
-                disabled={!isFormEnabled}
-                isDropdown={true}
-                options={yearlvl}
-              />
-            </div>
-          </div>
-          <div className='flex justify-between gap-4'>
-            <div className='w-1/2'>
-              <InputField
-                label='Official PLM Email'
-                value={PLMemail}
-                onChange={(e) => setPLMEmail(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-            <div className='w-1/2'>
-              <InputField
-                label='Official PLM Email'
-                value={personalEmail}
-                onChange={(e) => setPersonalEmail(e.target.value)}
-                disabled={!isFormEnabled}
-              />
-            </div>
-          </div>
-
           <div className='relative'>
             <button
               type='button'
