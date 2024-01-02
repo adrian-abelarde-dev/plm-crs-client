7ec857cae6effd51bca840a11ce2b947bf367a2d
urquico <kurtjacob.urquico@gmail.com>
2024-01-02 17:39:03 +0800

    [Feature][Kurt] - Added faculty view grades

---
+++ src/app/(private-routes)/faculty/view-grades/[courseCode]/[section]/page.jsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { fakeFacultyEncodingGrades } from '@/lib/constants/fake-data/faculty-encoding-grades';
+import { useEffect, useState } from 'react';
+
+import EditGrades from '../../../encoding-grades/[courseCode]/edit-grades';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+function ViewGrades({ params }) {
+  const [viewGrades, setViewGrades] = useState(fakeFacultyEncodingGrades);
+
+  useEffect(() => {
+  // Update whenever params.courseCode and params.section changes
+  const filteredData = fakeFacultyEncodingGrades.filter(
+    (data) => data.courseCode === params.courseCode && data.section === params.section
+  );
+  setViewGrades(filteredData);
+}, [params.courseCode, params.section]);
+
+
+  return (
+    <main className='p-6 flex flex-col'>
+      <div className='mt-12 mb-16'>
+        <h1 className='text-4xl font-medium'>View Grades</h1>
+      </div>
+
+      <div className='grid grid-cols-4 gap-4 mt-12 mx-24 place-items-center max-md:mx-0 max-md:grid-cols-1 max-md:justify-items-start max-md:p-6 max-md:border max-md:shadow rounded'>
+        <div className='flex flex-col gap-2'>
+          <h1 className='font-bold text-lg'>Course Code - Section</h1>
+          <p>
+            {params.courseCode} - {params.section}
+          </p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          <h1 className='font-bold text-lg'>Course Title</h1>
+          <p>CS Elective 2 (lab)</p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          <h1 className='font-bold text-lg'>Units</h1>
+          <p>3</p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          <h1 className='font-bold text-lg'>Term / A.Y</h1>
+          <p>1 / 2023</p>
+        </div>
+      </div>
+
+      {/* Table */}
+      <EditGrades studentsData={viewGrades} setGrades={setViewGrades} />
+
+      {/* Actions */}
+      <div className='mt-4 flex gap-4 justify-center'>
+        <Button variant='outline' asChild>
+          <Link href='/faculty/view-grades'>Cancel</Link>
+        </Button>
+        {viewGrades[0]?.students.length !== 0 && <Button>Save</Button>}
+      </div>
+    </main>
+  );
+}
+
+export default ViewGrades;
---
+++ src/app/(private-routes)/faculty/view-grades/page.jsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { fakeFacultyEncodingGrades } from '@/lib/constants/fake-data/faculty-encoding-grades';
+import Link from 'next/link';
+import { useState } from 'react';
+
+function ViewGradesPage() {
+  
+
+  const [courseCodeInput, setCourseCodeInput] = useState('');
+
+  return (
+    <main className='p-6 flex flex-col'>
+      <div className='mt-12 mb-16'>
+        <h1 className='text-4xl font-medium'>View Grades</h1>
+      </div>
+
+      <div className='w-[20rem] mb-10'>
+        <Label htmlFor='courseCode'>Search Course Code</Label>
+        <Input
+          id='courseCode'
+          placeholder='Search'
+          onChange={(e) => setCourseCodeInput(e.target.value)}
+          value={courseCodeInput}
+        />
+      </div>
+
+      {courseCodeInput !== '' ? (
+        <>
+          {/* Table Layout */}
+          <Table>
+            <TableHeader>
+              <TableRow className='font-bold'>
+                <TableCell>Course Code</TableCell>
+                <TableCell>Section</TableCell>
+                <TableCell>Course Title</TableCell>
+                <TableCell>Class Schedule</TableCell>
+                <TableCell>Instructor</TableCell>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {fakeFacultyEncodingGrades?.map((data, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell className='underline font-bold text-yellow-500'>
+                      <Link href={`/faculty/view-grades/${data.courseCode}/${data.section}`}>
+                        {data.courseCode}
+                      </Link>
+                    </TableCell>
+                    <TableCell>{data.section}</TableCell>
+                    <TableCell>{data.courseTitle}</TableCell>
+                    <TableCell>{data.classSchedule}</TableCell>
+                    <TableCell>{data.instructor}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </>
+      ) : (
+        <div className='w-full h-96 flex justify-center place-items-center text-zinc-400 '>
+          Please search for a Course Code
+        </div>
+      )}
+    </main>
+  );
+}
+
+export default ViewGradesPage;
